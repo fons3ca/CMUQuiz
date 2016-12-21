17cc53e806b41c006454c7dacf4dd7a9d2484cc6
fons3ca <n_fonseca@live.com.pt>
2016-12-21 17:44:35 +0000

    Stats view e main activity
    falta manifest

---
+++ app/src/main/java/quizdroid/estgf/ipp/pt/quizdroid/StatsActivity.java
@@ -0,0 +1,13 @@
+package quizdroid.estgf.ipp.pt.quizdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
